0846aa3b5f8f61247bce756126ce03cdd244088d
diego boy <diegolive5000@gmail.com>
2022-01-05 18:15:16 +0100

    set up complete, create mock contacts, add ejs for views, add contacts index view, add create new contact view and form

--- app.js
+++ app.js
@@ -1,11 +1,31 @@
-const express = require('express')
-const app = express()
-const port = 8080
+const express = require('express');
+const contactRouter = require('./routes/contacts')
+const app = express();
+const port = 8080;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.set('view engine', 'ejs')
+
+app.use('/contacts', contactRouter)
+
+app.get('/', function (req, res) {
+  const contacts = [{
+    firstName: 'Kyle',
+    lastName: 'WebDevSimplified',
+    birthday: new Date(),
+  },
+  {
+    firstName: 'Frida',
+    lastName: 'Spring',
+    birthday: new Date(),
+  },
+  {
+    firstName: 'Cydnie',
+    lastName: 'Seelenbinder',
+    birthday: new Date(),
+  }]
+  res.render('contacts/index', { contacts: contacts })
 })
 
 app.listen(port, () => {
   console.log(`Let's go Genie at http://localhost:${port}`)
-})
+});
